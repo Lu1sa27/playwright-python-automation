e4c9d1bb24bbb54b2354cbdcdd1dd2c113ad0567
Luisa Soto <luisamsotoagmail.com>
2026-03-06 12:03:25 -0600

    Implement LoginPage using Page Object Model

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,12 +1,19 @@
+from pages.base_page import BasePage
+
 class LoginPage(BasePage):
+    
     def __init__(self, page):        
         #Initialize the login page elements
         super().__init__(page)
-        self.username_input = "luisamsotoa@gmail.com"
-        self.password_input = "gvm-cjf6qzu6EWA!qyt"
-        self.login_button = "#login"
+        self.username_input = "#user-name"
+        self.password_input = "#password"
+        self.login_button = "#login-button"
+        self.error_message = "[data-test='error']"
 
     def login (self, username, password):
         self.page.fill(self.username_input, username)
         self.page.fill(self.password_input, password)
         self.page.click(self.login_button)
+    
+    def get_error_message(self):
+        return self.get_text(self.error_message)
