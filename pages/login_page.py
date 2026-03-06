6be25c6283ce432a33259e634ea0e7d607794f45
Luisa Soto <luisamsotoagmail.com>
2026-03-06 12:50:02 -0600

    Add comments on .py files

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,7 +1,7 @@
 from pages.base_page import BasePage
 
 class LoginPage(BasePage):
-    
+    #  Login page elements and methods
     def __init__(self, page):        
         #Initialize the login page elements
         super().__init__(page)
@@ -11,9 +11,11 @@ class LoginPage(BasePage):
         self.error_message = "[data-test='error']"
 
     def login (self, username, password):
+        # Perform login with given credentials
         self.page.fill(self.username_input, username)
         self.page.fill(self.password_input, password)
         self.page.click(self.login_button)
     
     def get_error_message(self):
+        # Get the error message text after failed login attempt
         return self.get_text(self.error_message)
--- tests/test_login.py
+++ tests/test_login.py
@@ -4,24 +4,21 @@ from pages.login_page import LoginPage
 def test_successful_login(page):
     # Open page
     page.goto("https://www.saucedemo.com")
-
+    #  Fill in credentials
     login_page = LoginPage(page)
-    
     login_page.login("standard_user", "secret_sauce")
     
-    # Validación
+    #  Assert that we have been redirected to the inventory page
     assert "saucedemo" in page.url
 
 def test_invalid_login(page):
-    # Abrir página
+    # Open page
     page.goto("https://www.saucedemo.com")
-
+    #  Fill in credentials
     login_page = LoginPage(page)
-
-    # Ejecutar login inválido
     login_page.login("invalid_user", "wrong_password")
-
-    # Validación del mensaje de error
+    # Get error message
     error = login_page.get_error_message()
 
+    # Assert that error message is present
     assert "Username and password do not match" in error
